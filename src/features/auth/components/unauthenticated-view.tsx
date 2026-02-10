0145927f0fe655cbca2d8ea5d3f5a5da76a6014b
Sayed Anowar <108403588+iamsayedanowar@users.noreply.github.com>
2026-02-10 16:56:51 +0530

    p-2

--- src/features/auth/components/unauthenticated-view.tsx
+++ src/features/auth/components/unauthenticated-view.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 export const UnauthenticatedView = () => {
     return (
-        <div className='flex items-center justify-center h-screen bg-background'>
+        <div className='flex items-center justify-center h-screen bg-background p-2'>
             <div className='w-full max-w-lg bg-muted'>
                 <Item variant='outline'>
                     <ItemMedia variant='icon'>
